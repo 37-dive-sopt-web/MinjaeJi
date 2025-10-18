d5d332a7be595dbb5f3c4cc66d256ff3fa7fd615
mimizae <jim400934@gmail.com>
2025-10-18 16:53:42 +0900

    feat: 돔 실습 폴더 생성

---
+++ Week2/Practice/todo-list/todo-list.js
@@ -0,0 +1,6 @@
+function keyCodeCheck() {
+  console.log(window.event); // window는 생략 가능
+}
+
+const button = document.querySelector(".button");
+button.addEventListener("click", () => {}); // 버튼 클릭했을 때
