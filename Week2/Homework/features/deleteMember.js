508d90bfaaedab4128758e6745b7031feeed0e8b
mimizae <jim400934@gmail.com>
2025-10-29 17:09:57 +0900

    refactor: handleDeleteMember 함수 내 변수명 명확히 수정

--- Week2/Homework/features/deleteMember.js
+++ Week2/Homework/features/deleteMember.js
@@ -7,7 +7,7 @@ export function handleDeleteMember(renderResultTable) {
   selectAll.addEventListener("change", (e) => {
     document
       .querySelectorAll('#membersTable tbody input[type="checkbox"]')
-      .forEach((cb) => (cb.checked = e.target.checked));
+      .forEach((checkbox) => (checkbox.checked = e.target.checked));
   });
 
   deleteBtn.addEventListener("click", () => {
@@ -17,21 +17,26 @@ export function handleDeleteMember(renderResultTable) {
     );
 
     if (selected.length === 0) {
-      alert("삭제할 멤버를 선택하세요!");
+      alert("삭제할 멤버를 선택하세요.");
       return;
     }
 
     if (!confirm("정말 삭제하시겠습니까?")) {
-      selected.forEach((cb) => (cb.checked = false));
+      selected.forEach((checkbox) => (checkbox.checked = false));
       selectAll.checked = false;
       return;
     }
 
-    const selectedIds = Array.from(selected).map((cb) => Number(cb.dataset.id));
-    const updated = membersData.filter((m) => !selectedIds.includes(m.id));
+    const selectedMemberIds = Array.from(selected).map((checkbox) =>
+      Number(checkbox.dataset.id)
+    );
 
-    setMembersData(updated);
-    renderResultTable(updated);
+    const updatedMembersData = membersData.filter(
+      (member) => !selectedMemberIds.includes(member.id)
+    );
+
+    setMembersData(updatedMembersData);
+    renderResultTable(updatedMembersData);
     selectAll.checked = false;
   });
 }
