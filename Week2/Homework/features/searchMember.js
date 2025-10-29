3111e9deb1dac75c2a5259965635ed4b16a83f60
mimizae <jim400934@gmail.com>
2025-10-29 17:41:50 +0900

    refactor: 검색 필터 input 값 에러 핸들링

--- Week2/Homework/features/searchMember.js
+++ Week2/Homework/features/searchMember.js
@@ -19,6 +19,29 @@ export function handleSearchMember(renderResultTable) {
     ] = searchInputs;
     const membersData = getMembersData();
 
+    if (englishNameInput.value && !/^[A-Za-z]+$/.test(englishNameInput.value)) {
+      alert("영문 이름은 영어만 입력할 수 있습니다.");
+      return;
+    }
+
+    if (
+      codeReviewGroupInput &&
+      (!/^[1-9]+$/.test(codeReviewGroupInput.value) ||
+        Number(codeReviewGroupInput.value) < 1 ||
+        Number(codeReviewGroupInput.value) > 9)
+    ) {
+      alert("금잔디 조는 1부터 9 사이의 정수만 입력 가능합니다.");
+      return;
+    }
+
+    if (
+      ageInput &&
+      (!/^\d+$/.test(ageInput.value) || Number(ageInput.value) <= 0)
+    ) {
+      alert("나이는 0보다 큰 정수만 입력할 수 있습니다.");
+      return;
+    }
+
     const filteredMembersData = membersData.filter((member) => {
       return (
         (nameInput.value === "" || member.name.includes(nameInput.value)) &&
