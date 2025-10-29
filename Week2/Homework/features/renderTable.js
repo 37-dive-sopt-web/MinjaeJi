e030ed8781fe92de9daf3080e31263d8fabd7122
mimizae <jim400934@gmail.com>
2025-10-29 17:17:01 +0900

    refactor: renderResultTable 함수 수정
    - a 태그에 rel="noopener norefferer" 속성 추가
    - 검색 결과 없을 시 인라인 스타일 추가
    - 변수명 명확히 수정

--- Week2/Homework/features/renderTable.js
+++ Week2/Homework/features/renderTable.js
@@ -3,23 +3,23 @@ export function renderResultTable(membersData) {
   tableBody.innerHTML = "";
 
   if (membersData.length === 0) {
-    tableBody.innerHTML = `<tr><td colspan="8">검색 결과가 없습니다.</td></tr>`;
+    tableBody.innerHTML = `<tr><td colspan="8" style="justify-contents: center">검색 결과가 없습니다.</td></tr>`;
     return;
   }
 
   membersData.forEach((member) => {
-    const row = `
+    const memberItem = `
       <tr>
         <td><input type="checkbox" data-id="${member.id}" /></td>
         <td>${member.name}</td>
         <td>${member.englishName}</td>
-        <td><a href="https://github.com/${member.github}" target="_blank">${member.github}</a></td>
+        <td><a href="https://github.com/${member.github}" target="_blank" rel="noopener norefferer">${member.github}</a></td>
         <td>${member.gender}</td>
         <td>${member.role}</td>
         <td>${member.codeReviewGroup}</td>
         <td>${member.age}</td>
       </tr>
     `;
-    tableBody.insertAdjacentHTML("beforeend", row);
+    tableBody.insertAdjacentHTML("beforeend", memberItem);
   });
 }
