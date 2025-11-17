836a2e4d84c12f02bb3f3bacc171920aab0eea06
mimizae <jim400934@gmail.com>
2025-11-18 00:40:17 +0900

    feat: 공통 Layout 적용 및 Outlet 기반 Header 조건부 표시 구현

--- Week4/Homework/src/App.tsx
+++ Week4/Homework/src/App.tsx
@@ -3,31 +3,39 @@ import {
   Navigate,
   RouterProvider,
 } from "react-router-dom";
+
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import MyPage from "./pages/MyPage";
 import Members from "./pages/Members";
+import Layout from "./layout/Layout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to="/login" replace />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/sign-up",
-    element: <SignUp />,
-  },
-  {
-    path: "/my-page",
-    element: <MyPage />,
-  },
-  {
-    path: "/members",
-    element: <Members />,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/login" replace />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "sign-up",
+        element: <SignUp />,
+      },
+      {
+        path: "my-page",
+        element: <MyPage />,
+      },
+      {
+        path: "members",
+        element: <Members />,
+      },
+    ],
   },
 ]);
 
---
+++ Week4/Homework/src/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { Outlet, useLocation } from "react-router-dom";
+import Header from "../components/Header/Header";
+
+export default function Layout() {
+  const location = useLocation();
+
+  const hideHeader = ["/login", "/sign-up"].includes(location.pathname);
+
+  return (
+    <>
+      {!hideHeader && <Header />}
+      <Outlet />
+    </>
+  );
+}
