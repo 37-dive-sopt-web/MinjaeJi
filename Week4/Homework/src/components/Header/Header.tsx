1a93ae862dcfabc0bf1e13f83349fdb145e7ebe4
mimizae <jim400934@gmail.com>
2025-11-18 08:18:45 +0900

    feat: 헤더 ui 구현

---
+++ Week4/Homework/src/components/Header/Header.tsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as styles from "./header.css";
+import hamburgerIcon from "@/assets/hamburger-icon.png";
+import closeIcon from "@/assets/x-icon.png";
+
+export default function Header() {
+  const [openSideBarMenu, setOpenSideBarMenu] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleNavClick = (path: string) => {
+    navigate(path);
+    setOpenSideBarMenu(false);
+  };
+
+  return (
+    <>
+      <header className={styles.headerContainer}>
+        <div className={styles.leftTitle}>마이페이지 😋</div>
+        {/* TODO: 로그인 성공 후 안녕하세요 지민재 님! 추가 */}
+
+        <nav className={styles.navMenu}>
+          <span
+            className={`${styles.navItem} ${
+              location.pathname === "/my-page" ? styles.activeNavItem : ""
+            }`}
+            onClick={() => handleNavClick("/my-page")}
+          >
+            내 정보
+          </span>
+
+          <span
+            className={`${styles.navItem} ${
+              location.pathname === "/members" ? styles.activeNavItem : ""
+            }`}
+            onClick={() => handleNavClick("/members")}
+          >
+            회원 조회
+          </span>
+
+          <span className={styles.navItem}>로그아웃</span>
+
+          <span className={styles.navItem}>회원 탈퇴</span>
+        </nav>
+
+        <div
+          className={styles.hamburger}
+          onClick={() => setOpenSideBarMenu(true)}
+        >
+          <img src={hamburgerIcon} width={16} height={16} alt="햄버거 아이콘" />
+        </div>
+      </header>
+
+      {openSideBarMenu && (
+        <div
+          className={styles.overlay}
+          onClick={() => setOpenSideBarMenu(false)}
+        />
+      )}
+
+      <aside
+        className={`${styles.sideMenu} ${
+          openSideBarMenu ? styles.sideMenuOpen : ""
+        }`}
+      >
+        <span
+          className={styles.closeButton}
+          onClick={() => setOpenSideBarMenu(false)}
+        >
+          <img src={closeIcon} width={16} height={16} />
+        </span>
+
+        <span
+          className={`${styles.navItem} ${
+            location.pathname === "/my-page" ? styles.activeNavItem : ""
+          }`}
+          onClick={() => handleNavClick("/my-page")}
+        >
+          내 정보
+        </span>
+
+        <span
+          className={`${styles.navItem} ${
+            location.pathname === "/members" ? styles.activeNavItem : ""
+          }`}
+          onClick={() => handleNavClick("/members")}
+        >
+          회원 조회
+        </span>
+
+        <span className={styles.navItem}>로그아웃</span>
+
+        <span className={styles.navItem}>회원 탈퇴</span>
+      </aside>
+    </>
+  );
+}
---
+++ Week4/Homework/src/components/Header/header.css.ts
@@ -0,0 +1,95 @@
+import { style } from "@vanilla-extract/css";
+
+export const overlay = style({
+  position: "fixed",
+  top: 0,
+  left: 0,
+  width: "100vw",
+  height: "100vh",
+  backgroundColor: "rgba(0, 0, 0, 0.19)",
+  zIndex: 10,
+});
+
+export const headerContainer = style({
+  width: "100%",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  padding: "20px 30px",
+  boxSizing: "border-box",
+  boxShadow: "0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)",
+  backgroundColor: "#dae9f9ff",
+});
+
+export const leftTitle = style({
+  fontSize: "20px",
+  fontWeight: 600,
+});
+
+export const navMenu = style({
+  display: "flex",
+  gap: "24px",
+  "@media": {
+    "(max-width: 768px)": {
+      display: "none",
+    },
+  },
+});
+
+const activeStyle = {
+  fontWeight: 600,
+  color: "#6caaedff",
+  textDecoration: "underline",
+  textDecorationColor: "#7db9f9ff",
+};
+
+export const navItem = style({
+  cursor: "pointer",
+  color: "#4d4d4dff",
+  fontSize: "15px",
+  transition: "color 0.2s ease, text-decoration-color 0.2s ease",
+  textDecoration: "none",
+
+  ":hover": activeStyle,
+});
+
+export const activeNavItem = style(activeStyle);
+
+export const hamburger = style({
+  display: "none",
+  cursor: "pointer",
+  fontSize: "24px",
+  "@media": {
+    "(max-width: 768px)": {
+      display: "block",
+    },
+  },
+});
+
+export const sideMenu = style({
+  position: "fixed",
+  top: 0,
+  right: 0,
+  width: "220px",
+  height: "100vh",
+  backgroundColor: "white",
+  boxShadow: "-4px 0 6px rgba(0,0,0,0.15)",
+  padding: "20px",
+  display: "flex",
+  flexDirection: "column",
+  gap: "40px",
+  transform: "translateX(100%)",
+  transition: "transform 0.3s ease",
+  zIndex: 11,
+});
+
+export const sideMenuOpen = style({
+  transform: "translateX(0)",
+});
+
+export const closeButton = style({
+  alignSelf: "flex-end",
+  marginBottom: "20px",
+  cursor: "pointer",
+  fontSize: "20px",
+});
